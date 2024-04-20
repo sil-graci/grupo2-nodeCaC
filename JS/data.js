7055960d217b3c66eb82aa99e21627188ce88d57
sil-graci <silviagraci@gmail.com>
2024-04-20 16:11:02 -0300

    corrección pág contacto.html y alimentos.html

--- JS/data.js
+++ JS/data.js
@@ -1,15 +1,75 @@
 var data=[
     {
         codigo:"cod1",
-        imagen:"../IMAGENES/delicias-gourmet.png",
-        nombre:"Delicias Gourmet En Tesis",
-        presio:"$8201"
+        imagen:"../IMAGENES/alimento1.png",
+        alt:"Galleta_perro",
+        nombre:"CroquiBones",
+        descripcion:"Deliciosas galletas nutritivas, perfectas para mantener a tu perro feliz y saludable.",
+        precio:"$3500"
     },
     {
         codigo:"cod2",
-        imagen:"../IMAGENES/sabor-del-bosque.png",
-        nombre:"Sabor del bosque",
-        descripcion:"$7001"
-    }
+        imagen:"../IMAGENES/alimento2.png",
+        alt:"Galleta_perro",
+        nombre:"CroquiGreen",
+        descripcion:"Exquisitas galletas repletas de nutrientes y vegetales frescos para mantener a tu perro fuerte y saludable.",
+        precio:"$4100"
+    },
+
+    {
+        codigo:"cod3",
+        imagen:"../IMAGENES/alimento3.png",
+        alt:"Nutriente-gato",
+        nombre:"Jasmine Rice",
+        descripcion:"Fórmula enriquecida con vitaminas y minerales que promueven la salud y el bienestar de tu gato.",
+        precio:"$8900"
+    },
+
+    {
+        codigo:"cod4",
+        imagen:"../IMAGENES/alimento4.png",
+        alt:"Mezcla-gatos",
+        nombre:"Fresh Kisses",
+        descripcion:"Mezcla perfecta de texturas y sabores que mantendrán a tu gato saludable.",
+        precio:"$3400"
+    },
+
+    {
+        codigo:"cod5",
+        imagen:"../IMAGENES/alimento5.png",
+        alt:"Bocaditos-gatos",
+        nombre:"Moist Meals",
+        descripcion:"Deliciosos bocados con sabor a atún que satisfacen el paladar exigente de tu gato.",
+        precio:"$2200"
+    },
+
+    {
+        codigo:"cod6",
+        imagen:"../IMAGENES/alimento6.png",
+        nombre:"Bark Bites",
+        alt:"Golosina_perro",
+        descripcion:"Pequeñas golosinas crujientes; un bocadito delicioso para tu perro.",
+        precio:"$6900"
+    },
+
+    {
+        codigo:"cod7",
+        imagen:"../IMAGENES/alimento7.png",
+        alt:"Gourmet_gato",
+        nombre:"Funny Cat",
+        descripcion:"Una mezcla gourmet de ingredientes seleccionados para satisfacer los gustos refinados de tu felino.",
+        precio:"$1800"
+    },
+
+    {
+        codigo:"cod8",
+        imagen:"../IMAGENES/alimento8.png",
+        alt:"Alimento_aves",
+        nombre:"Early Bird",
+        descripcion:"Alimento balanceado que cuida las plumas y la salud general de tu ave",
+        precio:"$2600"
+    },
+
+    
 
 ];
